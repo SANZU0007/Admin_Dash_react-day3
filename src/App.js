e88bdc317ba224d8afb4387a0979ee669f0e259a
sanjay <LENOVO@DESKTOP-AKPL9L4.(none)>
2023-10-24 10:41:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./compenents/dashboard";
+
+import Buttons from "./compenents/buttons";
+import Cards from "./compenents/cards";
+import Colors from "./compenents/colors";
+import Border from "./compenents/border";
+import Error from "./compenents/error";
+import Animation from "./compenents/animation";
+import Others from "./compenents/others";
+import Login from "./compenents/loginPage";
+import Register from "./compenents/registerPage";
+import Reset from "./compenents/forgotPassword";
+import Table from './compenents/table';
+import BlankPage from "./compenents/blank";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/buttons" element={<Buttons />} />
+          <Route path="/cards" element={<Cards />} />
+          <Route path="/colors" element={<Colors />} />
+          <Route path="/borders" element={<Border />} />
+          <Route path="/animation" element={<Animation />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/others" element={<Others />} />
+          <Route path="/nopage" element={<Error />} />
+          <Route path="/blank" element={<BlankPage />} />
+          <Route path='/table' element={<Table/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
